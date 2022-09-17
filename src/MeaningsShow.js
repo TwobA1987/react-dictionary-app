afb9e3aa8bebca84c6d51a6be2d1b496b3862c6c
tbrsi <99534701+tbrsi@users.noreply.github.com>
2022-09-17 13:25:02 +0430

    made some css edit

--- src/MeaningsShow.js
+++ src/MeaningsShow.js
@@ -3,11 +3,11 @@ export default function MeaningsShow(props) {
   return (
     <div className="MeaningsShow">
       <h4 className="text-secondary">{props.data.partOfSpeech}</h4>
-      <ul>
+      <ul className="list-group list-group-flush">
         {props.data.definitions.map(function (definition, index) {
           return (
             <div key={index}>
-              <li>
+              <li className="list-group-item">
                 {definition.definition}
                 <div>
                   <em className="text-secondary">{definition.example}</em>
