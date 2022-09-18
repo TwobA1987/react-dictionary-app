b75b74ae9a9fef6da23cba78c871da4268a1abaa
tbrsi <99534701+tbrsi@users.noreply.github.com>
2022-09-18 23:52:02 +0430

    add photos section

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import ResultShow from "./ResultShow";
+import PhotosShow from "./PhotosShow";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("fantastic");
   let [Results, setResults] = useState(null);
   let [Loaded, setLoaded] = useState(false);
+  let [Photos, SetPhotos] = useState(null);
   function HandleKeyWordChange(event) {
     event.preventDefault();
     setKeyWord(event.target.value);
   }
-  function HandleResponse(response) {
+  function HandleDicResponse(response) {
     setResults(response.data[0]);
+  }
+  function HandlePexelResponse(response) {
+    SetPhotos(response.data.photos);
   }
   function Search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyWord}`;
-    axios.get(apiUrl).then(HandleResponse);
+    axios.get(apiUrl).then(HandleDicResponse);
+    let apiKey = "563492ad6f917000010000010d70e15c5b224b7dbdddf36017c9edef";
+    let PexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyWord}&per_page=9`;
+    let config = { Authorization: apiKey };
+    axios.get(PexelsApiUrl, { headers: config }).then(HandlePexelResponse);
   }
   function HandleSubmit(event) {
     event.preventDefault();
@@ -41,6 +50,7 @@ export default function Dictionary() {
           </form>
         </section>
         <ResultShow data={Results} />
+        <PhotosShow data={Photos} />
       </div>
     );
   } else {
---
+++ src/PhotosShow.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./PhotosShow.css";
+export default function PhotosShow(props) {
+  if (props.data) {
+    return (
+      <section className="PhotosShow">
+        <div className="row">
+          {props.data.map(function (Photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={Photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={Photo.src.landscape}
+                    className="img-fluid"
+                    alt={Photo.alt}
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
